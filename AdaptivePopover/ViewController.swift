97ec6b0539fb20f99eda7ec86da6bd6d312914fd
frogcjn <frogcjn@163.com>
2014-06-08 22:40:56 -0700

    Initial Commit

---
+++ AdaptivePopover/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaptivePopover
+//
+//  Created by 曹剑楠 on 14/6/8.
+//  Copyright (c) 2014年 Cao Jiannan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AdaptivePopoverTests/AdaptivePopoverTests.swift
@@ -0,0 +1,35 @@
+//
+//  AdaptivePopoverTests.swift
+//  AdaptivePopoverTests
+//
+//  Created by 曹剑楠 on 14/6/8.
+//  Copyright (c) 2014年 Cao Jiannan. All rights reserved.
+//
+
+import XCTest
+
+class AdaptivePopoverTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
